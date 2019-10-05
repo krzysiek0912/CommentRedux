187f086fa6a9c7acc5494b22101b495fd4ee0b93
Krzysztof Branecki <krzysztof@branecki.net>
2019-10-05 13:23:30 +0200

    Add DevTools

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import CommentsListContainer from '../../container/CommentsListContainer';
 import AddCommentContainer from '../../container/AddCommentContainer';
+import DevTools from "../../DevTools";
 
 const App = () => {
   return (
     <div className="App">
       <AddCommentContainer/>
       <CommentsListContainer />
+      <DevTools />
     </div>
   );
 };
--- src/store.js
+++ src/store.js
@@ -1,6 +1,16 @@
 import { createStore } from 'redux';
-import reducer from "./reducer";
+// import { createStore, applyMiddleware } from 'redux';
+// import { createLogger } from 'redux-logger';
 
- const store = createStore(reducer);
+import reducer from "./reducer";
+import DevTools from './DevTools';
+
+
+
+const store = createStore(reducer, 
+  DevTools.instrument());
+
+// const logger = createLogger();
+// const store = createStore(reducer, applyMiddleware(logger));
 
  export default store
